b6653a95522003a490c4ecd5d9937b2c8eaf8bfd
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-11 13:07:47 -0400

    Added directions and filtering to map

--- js/cities.js
+++ js/cities.js
@@ -6,16 +6,25 @@ app.controller('citiesCtrl', function($scope){
 	var myLatLng = {lat: 40.6643, lng: -73.9385};
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: myLatLng,
-		zoom: 8	
+		zoom: 3	
 	});
 
-	var markers = [];
+	var markers = [];	
 
 	function createMarker(city){
+		var icon = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569';
+		
+		if(city.yearRank == 1){
+			icon = 'img/1.png';
+		}else if(city.yearRank == 39){
+			icon = 'img/atl.png';
+		}
+
 		var marker = new google.maps.Marker({
 			position: {lat: city.lat, lng: city.lon},
 		    	map: map,
-		    	title: city.city
+		    	title: city.city,
+		    	icon: icon
 		});
 
 		var infowindow = new google.maps.InfoWindow({
@@ -29,12 +38,37 @@ app.controller('citiesCtrl', function($scope){
 		markers.push(marker);
 	}
 
-	for(var i = 0; i < cities.length; i++){
-	       createMarker(cities[i]);
+	for(var i = 0; i < $scope.cities.length; i++){
+	       createMarker($scope.cities[i]);
 	}
 
 	$scope.triggerClick = function(index){
 		google.maps.event.trigger(markers[index], "click");
-		map.setCenter( markers[index].position);
-	}	
+		map.setCenter(markers[index].position);
+	}
+
+	$scope.updateMarkers = function(){
+		for(var i = 0; i < markers.length; i++){
+			markers[i].setMap(null);
+		}
+
+		for(var i = 0; i < $scope.filteredCities.length; i++){
+			createMarker($scope.filteredCities[i]);
+		}
+	}
+
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+	directionsDisplay.setMap(map);
+		directionsService.route({
+		origin: 'Atlanta, GA',
+		destination: 'New York, NY',
+		travelMode: 'DRIVING'
+	}, function(response, status){
+		if(status === 'OK'){
+			directionsDisplay.setDirections(response);
+		}else{
+			alert("Failed due to " + status);
+		}
+	});		
 });
